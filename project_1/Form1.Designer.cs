b0173625f7d6467e623187d328f0d4d595b463a8
22-46838-1 <22-46838-1@student.aiub.edu>
2023-11-22 09:36:33 +0600

    up-1

--- project_1/Form1.Designer.cs
+++ project_1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.btn_sign_up = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.Password_field = new System.Windows.Forms.TextBox();
@@ -43,6 +44,7 @@
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.btn_sign_up);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.Password_field);
@@ -56,6 +58,15 @@
             this.panel1.Size = new System.Drawing.Size(324, 365);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // btn_sign_up
             // 
@@ -160,6 +171,7 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox Password_field;
         private System.Windows.Forms.Button btn_sign_up;
+        private System.Windows.Forms.Button button2;
     }
 }
 
